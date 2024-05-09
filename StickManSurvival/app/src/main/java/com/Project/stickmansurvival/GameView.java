ba9e69ba0963788c5cac4a0380b799330558af2d
jun2hg <tbd05042@gmail.com>
2024-05-09 23:49:10 +0900

    GameActivity,GameView생성
    MainActivity에서  GameActivity가 실행되도록 함

---
+++ StickManSurvival/app/src/main/java/com/Project/stickmansurvival/GameView.java
@@ -0,0 +1,10 @@
+package com.Project.stickmansurvival;
+
+import android.content.Context;
+import android.view.View;
+
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+}
--- StickManSurvival/app/src/main/java/com/Project/stickmansurvival/MainActivity.java
+++ StickManSurvival/app/src/main/java/com/Project/stickmansurvival/MainActivity.java
@@ -1,12 +1,11 @@
 package com.Project.stickmansurvival;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnStartGame(View view){
+        startActivity(new Intent(this, GameActivity.class));
     }
 }
